451ddf02931a8b2dd5bb48608ecf0368e16e9181
sailfish009 <sailfish009@gmail.com>
2017-12-19 11:03:58 +0900

    implement dumb writer

--- wed/wedView.cpp
+++ wed/wedView.cpp
@@ -2,26 +2,55 @@
 #include "wedView.h"
 #include <string>
 
-long g_posx = 0;
+static POINT p = { 0 };
+static int char_w = 0;
+static DWORD char_x;
+static DWORD char_y;
 
-LRESULT CWedView::OnChar(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+
+LRESULT CWedView::OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 {
-  //printf("[wParam: 0x%02x]\n", wParam);
-  g_posx += 10;
-  POINT p = { g_posx,10 };
-  print((UINT8&)wParam, p);
+  m_hdc = GetDC();
+  GetTextMetrics(m_hdc, &m_tm);
+  ReleaseDC(m_hdc);
+  char_x = m_tm.tmAveCharWidth;
+  char_y = m_tm.tmHeight;
+
+  printf("x: %d  y: %d\n", char_x, char_y);
   return 0;
 }
 
-LRESULT CWedView::OnPaint(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+LRESULT CWedView::OnChar(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+{
+  switch (wParam)
+  {
+  case 8:   
+  { 
+    p.x -= char_w; 
+    RECT rect = { p.x, p.y, p.x + char_x, p.y + char_y }; 
+    InvalidateRect(&rect); 
+  }  
+  break;
+
+  default:  
+  {
+    m_hdc = GetDC();
+    GetCharWidth32(m_hdc, (UINT)wParam, (UINT)wParam, &char_w);
+    CClientDC pDC(m_hWnd);
+    pDC.TextOut(p.x, p.y, (LPCTSTR)&wParam);
+    ReleaseDC(m_hdc);
+    p.x += char_w;
+  }
+    break;
+  }
+  return 0;
+}
+
+LRESULT CWedView::OnPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 {
   CPaintDC dc(m_hWnd);
   return 0;
 }
 
-void CWedView::print(UINT8 &w, POINT &p)
-{
-  std::wstring str{ w };
-  CClientDC pDC(m_hWnd);
-  pDC.TextOut(p.x, p.y, str.c_str());
-}
+
+
--- wed/wedView.h
+++ wed/wedView.h
@@ -18,6 +18,7 @@ public:
 	BEGIN_MSG_MAP(CWedView)
 		MESSAGE_HANDLER(WM_PAINT, OnPaint)
     MESSAGE_HANDLER(WM_CHAR, OnChar)
+    MESSAGE_HANDLER(WM_CREATE, OnCreate)
   END_MSG_MAP()
 
 // Handler prototypes (uncomment arguments if needed):
@@ -25,9 +26,9 @@ public:
 //	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 //	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
 
-  void print(UINT8 &w, POINT &p);
-
-
+  HDC m_hdc;
+  TEXTMETRIC m_tm;
+  LRESULT OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
   LRESULT OnChar(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
   LRESULT OnPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
 };

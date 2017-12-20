e20b25708e485f6aa4550fc42094c36942f89499
sailfish009 <sailfish009@gmail.com>
2017-12-20 15:51:20 +0900

    code clean

--- wed/wedView.cpp
+++ wed/wedView.cpp
@@ -7,16 +7,17 @@ static int line_n = 0;
 static int char_w = 0;
 static long char_x;
 static long char_y;
-static POINT p = { 0 };     //current position
+static POINT p = { 0 };                                                                      //current position
 
 static BOOL line_changed = 0;
+static BOOL wed_mode = 0;                                                        // 0: edit mode,  1: save mode
 
 typedef struct
 {
-  long x;                              // character x position
-  long y;                              // character y position
-  UINT8 c;                           // character content
-  UINT8 w;                          // character width
+  long x;                                                                                                // character x position
+  long y;                                                                                                // character y position
+  UINT8 c;                                                                                            // character content
+  UINT8 w;                                                                                           // character width
 } CH ;
 
 CH ch = { 0 };
@@ -44,71 +45,42 @@ LRESULT CWedView::OnChar(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*/, BOOL& 
   {
     if (p.x < 1) return 1;
 
-    if ( (line_array.size() == 0)  || (line_array.size() < (p.y +1) ) )
-    {
-      line_array.push_back(line);
-    }
-
+    if ( (line_array.size() == 0)  || (line_array.size() < (size_t)(p.y +1) ) ) line_array.push_back(line);
     std::list <std::list<CH>>::iterator it = std::next(line_array.begin(), p.y);
-    if (line_changed) 
-    { 
-      line_changed = 0; 
-      it->swap(line); 
-    }
+    if (line_changed) { line_changed = 0; it->swap(line); }
 
     int count = 0;
     std::list<CH>::iterator line_a = it->begin();
     std::list<CH>::iterator line_b = it->begin();
-    int element_n = it->size() -1;
+    int e_n = it->size() -1;
     for (auto i = it->begin(); i != it->end(); i++)
     {
-      if (count >= element_n)
-      {
-        CH ch = (*i);
-        p.x -= ch.w;
-        RECT rect = { ch.x, ch.y*char_y, ch.x + ch.w, ch.y*char_y + char_y };
-        InvalidateRect(&rect);
-      }
+      if (count >= e_n) { CH ch = (*i); p.x -= ch.w;  RECT rect = { ch.x, ch.y*char_y, ch.x + ch.w, ch.y*char_y + char_y };  InvalidateRect(&rect);  }
       ++count;
     }
-
-    if (p.x > 0)
-    {
-      std::advance(line_a, element_n);
-      std::advance(line_b, element_n + 1);
-      it->erase(line_a, line_b);
-      line = (*it);
-    }
-    else
-    {
-      line.clear();
-      line_array.erase(it);
-      p.x = 0;
-    }
-
+    if (p.x > 0) { std::advance(line_a, e_n); std::advance(line_b, e_n + 1); it->erase(line_a, line_b); line = (*it); }
+    else { line.clear(); line_array.erase(it); p.x = 0; }
   }  
   break;
 
   // enter
   case 0x0D:   
-  { 
-    p.x = 0;
-    p.y += 1;
-    if (p.y > line_n)
-    {
-      line_array.push_back(line);
-      line_n = p.y;
-      line.clear();
-    }
-    else
-    {
-    }
-  }  
-  break;
+    p.x = 0,  p.y += 1;
+    if (p.y > line_n) { line_array.push_back(line); line_n = p.y; line.clear(); }
+    break;
 
   // ESC
   case 0x1B:   
   { 
+    wed_mode = !wed_mode;
+
+    switch (wed_mode)
+    {
+    case 0:break;
+    default:
+      break;
+    }
+
   }  
   break;
 
@@ -135,10 +107,9 @@ LRESULT CWedView::OnKeyDown(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*/, BOO
   {
   case VK_LEFT:       
   {
-    if (p.x == 0)              return 1;
+    if (p.x == 0)  return 1;
     int char_w = 0;
-    for (auto i = line.begin(); i != line.end(); i++)
-      if (p.x == (*i).x) { char_w = (*i).w; break; }
+    for (auto i = line.begin(); i != line.end(); i++) if (p.x == (*i).x) { char_w = (*i).w; break; }
     p.x -= char_w;
   }
     break;
@@ -146,25 +117,13 @@ LRESULT CWedView::OnKeyDown(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*/, BOO
   case VK_RIGHT:    
   {
     int char_w = 0;
-    for (auto i = line.begin(); i != line.end(); i++)
-      if (p.x == (*i).x) { char_w = (*i).w; break; }
+    for (auto i = line.begin(); i != line.end(); i++) if (p.x == (*i).x) { char_w = (*i).w; break; }
     p.x += char_w;
   }
     break;
 
-  case VK_UP:
-  {
-    if (p.y == 0)              return 1;
-    p.y -= 1;
-  }
-    break;
-
-  case VK_DOWN:
-  {
-    if (p.y == line_n)    return 1;
-    p.y += 1;
-  }
-    break;
+  case VK_UP:         { if (p.y == 0) return 1;    p.y -= 1; }           break;
+  case VK_DOWN:  { if (p.y == line_n) return 1;  p.y += 1; }  break;
   }
   return 0;
 }
